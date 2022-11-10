ac8b13c66246fcdb2b293e2548e26cc7bf030e5e
최영빈 <bin778@naver.com>
2022-11-10 21:32:51 +0900

    day41

--- frontend/myserver/server.js
+++ frontend/myserver/server.js
@@ -1,26 +1,33 @@
-
 // node_modules의 express 패키지를 가져온다.
-var express = require("express");
-
-// app이라는 변수에 express 함수의 값을 저장한다.
+const express = require("express");
+const path = require("path");
 var app = express();
 
-// 환경 변수에서 port를 가져온다. 그러나 환경변수 값이 없으면 5050 포트를 지정한다.
-var port = app.listen(process.env.PORT || 5050);
+// 메인 페이지 접속시 html 응답하는 방법
 
-// REST API의 한가지 종류인 GET request를 정의하는 부분이다.
-// app.get이라고 작성했기 때문에 get 요청으로 정의가 되고
-// app.post로 작성할 경우 post 요청으로 정의가 된다.
-// REST API의 종류(get, post, put, delete 등등)을 사용하여 end point를 작성할 수 있다.
+// 미들웨어 : html, css, js, img 파일들이 담긴 곳 명시
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get("/", (req, res) => {
-    // console.log(req);
-    console.log("/ 루트로 요청이 왔습니다.");
-
-    // 요청에 대한 응답
-    res.send("<h1>Express server Start</h1>");
+    console.log(__dirname);
+    console.log(path.join(__dirname, 'public/main.html'));
+    
+    res.sendFile(path.join(__dirname, 'public/main.html'));
 });
 
+// /hello 접속했을 때 hello.html을 보여주고 싶다.
+app.get("/hello", (req, res) => {
+    console.log(__dirname);
+
+    res.sendFile(path.join(__dirname, 'public/hello.html'));
+});
+
+app.get("/hello/test", (req, res) => {
+   res.sendFile(path.join(__dirname, 'public/test.html')); 
+});
+
+const http = require("http").createServer(app);
 // express 서버를 실행할 때 필요한 포트 정의 및 실행시 callback 함수를 받는다.
-app.listen(port, () => {
+app.listen(5050, () => {
     console.log("start! express server!");
 });
