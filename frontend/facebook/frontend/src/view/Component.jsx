05da9a62a1c04e9ed447c4acb28b4031d04e4019
최영빈 <bin778@naver.com>
2022-11-29 21:47:28 +0900

    day47

---
+++ frontend/facebook/frontend/src/view/Component.jsx
@@ -0,0 +1,32 @@
+export const Input = (props) => {
+    const {type, name, placeholder, onChange} = props;
+    return <input
+    className="my-input"
+    type={type}
+    name={name}
+    placeholder={placeholder}
+    onChange={onChange}
+    />
+}
+
+export const Image = (props) => {
+    const {src, alt} = props;
+
+    return <div className="my-image">
+        <img src={src} alt={alt}></img>
+    </div>
+}
+
+export const Title = (props) => {
+    return <h1 className="my-title">{props.text}</h1>
+}
+
+export const Linebar = (props) => {
+    return <div className="my-line"></div>
+}
+
+export const Button = (props) => {
+    const {text, type, onClick} = props;
+    console.log(props);
+    return <button className={`my-button ${type}`} onClick={onClick}>{text}</button>
+}
