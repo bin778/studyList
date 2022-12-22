fba60f2329489829ea305fca04119248cfaa859b
최영빈 <bin778@naver.com>
2022-12-22 21:46:19 +0900

    mariadb setting

--- frontend/facebook/backend/server.js
+++ frontend/facebook/backend/server.js
@@ -1,25 +1,51 @@
 const express = require("express");
-const path = require("path")
-const app = express()
+const path = require("path");
+const app = express();
 
 // 메인 페이지 접속시 html 응답하는 방법
 
 // 미들웨어 : html, css, js, img 파일들이 담긴 곳 명시
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-})
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 // 바디파서 미들웨어 설정
-const bodyparser = require('body-parser')
-app.use(bodyparser.urlencoded({extended: false}))
-app.use(bodyparser.json())
+const bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+const moment = require("moment");
+const logger = (req, res, next) => {
+  req.reqtime = moment().format("YYYY-MM-DD HH:mm:ss.sss");
+  console.log(
+    `=================> [${req.reqtime}][${req.method}${req.url} call!]`
+  );
+
+  // 콘솔창에 색상을 변경해주는 이스케이브 문자를 추가해보자(마지막엔 원래 상태로)
+  console.log(
+    "\x1b[36m%s",
+    `===========> [${req.reqtime}][${req.method}]${req.url} call!`,
+    "\x1b[37m"
+  );
+
+  req.query &&
+    Object.keys(req.query).length &&
+    console.log(`    ====> request query = `, req.query);
+  req.body &&
+    Object.keys(req.body).length &&
+    console.log(`     ====> request body = `, req.body);
+
+  next();
+};
+
+app.use(logger);
 
 const api = require("./src/api/index");
-app.use("/api", api)
+app.use("/api", api);
 
-const http = require("http").createServer(app)
+const http = require("http").createServer(app);
 http.listen(8080, () => {
-  console.log("server listen start : 8080")
-})
+  console.log("server listen start : 8080");
+});
--- frontend/facebook/backend/src/api/index.js
+++ frontend/facebook/backend/src/api/index.js
@@ -1,108 +1,103 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const mysql = require("./maria");
 
 // /api/login POST 데이터를 전달 받는다.
 router.post("/login", (req, res) => {
-  console.log("=================> [POST]/api/login call!!!")
+  console.log(req.body);
 
-  console.log(req.body)
+  const { userid, password } = req.body;
 
-  const {userid, password} = req.body
-  if(userid === "sentron" && password === "1234") {
-    res.send({result: "success"})
+  mysql.selectUsers("", (result) => {
+    console.log(result);
+  });
+
+  if (userid === "sentron" && password === "1234") {
+    res.send({ result: "success" });
   } else {
-    res.send({result: "fail"})
+    res.send({ result: "fail" });
   }
-
-})
+});
 
 // /api/regist POST 데이터를 전달 받는다.
 router.post("/regist", (req, res) => {
-  console.log("=================> [POST]/api/regist call!!!")
+  console.log(req.body);
 
-  console.log(req.body)
+  const { name, userid, password, year, month, day, gender } = req.body;
 
-  const {name, userid, password, year, month, day, gender} = req.body
-
-  if(name && userid && password && year && month && day && gender) {
-    res.send({result: "success"})
+  if (name && userid && password && year && month && day && gender) {
+    res.send({ result: "success" });
   } else {
-    res.send({result: "fail"})
+    res.send({ result: "fail" });
   }
-})
+});
 
 router.get("/identify", (req, res) => {
-  console.log("=================> [GET]/api/identify call!!!!");
+  console.log(req.require);
 
-  console.log(req.require)
+  const { email } = req.query;
 
-  const {email} = req.query
-
-  if(email == "sentron@email.com") {
-    res.send({result: "sentron"});
-  } else if(email === "aaa@email.com") {
-    res.send({result: "aaa123"});
-  } else if(email === "bbb@email.com") {
-    res.send({result: "bbb123"});
+  if (email == "sentron@email.com") {
+    res.send({ result: "sentron" });
+  } else if (email === "aaa@email.com") {
+    res.send({ result: "aaa123" });
+  } else if (email === "bbb@email.com") {
+    res.send({ result: "bbb123" });
   } else {
-    res.send({result: "fail", text: "계정이 존재하지 않습니다."});
+    res.send({ result: "fail", text: "계정이 존재하지 않습니다." });
   }
-})
+});
 
 router.delete("/user", (req, res) => {
-  console.log("=================> [GET]/api/user call!!!!");
+  const { email, userid } = req.query;
 
-  const {email, userid} = req.query;
-
-  if(email === "sentron@email.com" && userid === "sentron") {
-    res.send({result: "success"});
+  if (email === "sentron@email.com" && userid === "sentron") {
+    res.send({ result: "success" });
   } else {
-    res.send({result: "fail"});
+    res.send({ result: "fail" });
   }
-})
+});
 
 const array = [
   {
-      no: 1,
-      title: "에듀윌",
-      subtitle: "🚨기간한정 특별 이벤트🚨 초시생 필수템, 만화입문서 무료배포!",
-      tags: "#합격자수1위 #에듀윌 #공인중개사",
-      url: "EDUWILL.NET",
-      text: "입문교재 선착순 무료신청☞ 합격자 수 1위 에듀윌 공인중개사",
-      image: "/images/game-1.jpg",
-      likecount: 1
+    no: 1,
+    title: "에듀윌",
+    subtitle: "🚨기간한정 특별 이벤트🚨 초시생 필수템, 만화입문서 무료배포!",
+    tags: "#합격자수1위 #에듀윌 #공인중개사",
+    url: "EDUWILL.NET",
+    text: "입문교재 선착순 무료신청☞ 합격자 수 1위 에듀윌 공인중개사",
+    image: "/images/game-1.jpg",
+    likecount: 1,
   },
   {
-      no: 2,
-      title: "코리아아이티",
-      subtitle: "🚨기간한정 특별 이벤트🚨 프론트엔드 5개월차 수업!",
-      tags: "#합격자수1위 #코리아아이티 #프론트엔드",
-      url: "KOREAIT.NET",
-      text: "녹화 동영상 무료 제공!☞ 합격자 수 1위 에듀윌 공인중개사",
-      image: "/images/game-2.jpg",
-      likecount: 2
-  }
-]
+    no: 2,
+    title: "코리아아이티",
+    subtitle: "🚨기간한정 특별 이벤트🚨 프론트엔드 5개월차 수업!",
+    tags: "#합격자수1위 #코리아아이티 #프론트엔드",
+    url: "KOREAIT.NET",
+    text: "녹화 동영상 무료 제공!☞ 합격자 수 1위 에듀윌 공인중개사",
+    image: "/images/game-2.jpg",
+    likecount: 2,
+  },
+];
 
 router.get("/home", (req, res) => {
-  console.log("=================> [GET]/api/home call!!!!");
   console.log(req.query);
 
-  res.send({result: array});
-})
+  res.send({ result: array });
+});
 
 router.put("/home/like", (req, res) => {
-  console.log("=================> [GET]/api/home/like call!!!!");
   console.log(req.body);
 
-  const {no, like} = req.body;
+  const { no, like } = req.body;
 
-  const data = array.find(item => item.no === no)
+  const data = array.find((item) => item.no === no);
   data.likecount = Number(data.likecount) + Number(like);
 
   console.log(array);
 
-  res.send({result: "success"});
-})
+  res.send({ result: "success" });
+});
 
 module.exports = router;
