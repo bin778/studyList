97199f43d82d1243e7f8bfd5c35aded9f3adcc8d
CYI3 <bin778@naver.com>
2024-05-20 14:25:13 +0900

    feat Person.ts

---
+++ TypeScript/ex/Type/Person.ts
@@ -0,0 +1,28 @@
+class Person {
+  name: String;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Developer {
+  name: string;
+  age: number;
+  sleepTime: number;
+
+  constructor(name: string, age: number, sleepTime: number) {
+    this.name = name;
+    this.age = age;
+    this. sleepTime = sleepTime;
+  }
+}
+
+function greet(p: Person) {
+  console.log(`Hello, I'm ${p.name}`);
+}
+
+const developer = new Developer("zig", 20, 7);
+greet(developer);
