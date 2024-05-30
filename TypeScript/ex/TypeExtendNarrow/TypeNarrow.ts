4195edb4d0f1b0c8d4f154bb81774a68ecbffee4
CYI3 <bin778@naver.com>
2024-05-30 13:55:35 +0900

    fix TypeNarrow.ts

--- TypeScript/ex/TypeExtendNarrow/TypeNarrow.ts
+++ TypeScript/ex/TypeExtendNarrow/TypeNarrow.ts
@@ -17,3 +17,116 @@ interface Range {
 export function convertToRange(selected?: Date | Range): Range | undefined {
   return selected instanceof Date ? {start: selected, end: selected} : selected;
 }
+
+// 객체의 속성이 있는 없는지에 따른 구분: in 연산자 활용하기
+interface BasicNoticeDialogProps {
+  noticeTitle: string;
+  noticeBody: string;
+}
+
+interface NoticeDialogWithCookieProps extends BasicNoticeDialogProps {
+  cookieKey: string;
+  noForADay?: boolean;
+  neverAgain?: boolean;
+}
+
+export type NoticeDialogProps = BasicNoticeDialogProps | NoticeDialogWithCookieProps;
+
+const NoticeDialog: React.FC<NoticeDialogProps> = (props) => {
+  if('cookieKey' in props) return <NoticeDialogWithCookie {...props} />;
+  return <NoticeDialogBase {...props} />;
+}
+
+// is 연산자로 사용자 정의 타입 가드 만들어 활용하기
+function isString(value: unknown) value is string {
+  return typeof value === 'string';
+}
+
+function logValueIfExists(value: string | null | undefined) {
+  if(isString(value)) {
+ 	// ✅ OK
+    value.toString();    
+  }else {
+    console.log(`Value does not exist: ${value}`);
+  }
+}
+
+// 에러 정의 - 타입스크립트에서는 별도의 타입 에러를 뱉지 않아 알 수 없다는 문제점이 있다.
+type TextError = {
+  errorCode: string;
+  errorMessage: string;
+}
+
+type ToastError = {
+  errorCode: string;
+  errorMessage: string;
+  toastShowDuration: number;
+}
+
+type AlertError = {
+  errorCode: string;
+  errorMessage: string;
+  onConfirm: () => void;
+}
+
+type ErrorFeedbackType = TextError | ToastError | AlertError;
+
+// 식별할 수 있는 유니온(Discriminated Unions)
+type TextError = {
+  errorType: 'TEXT',
+  errorCode: string;
+  errorMessage: string;
+}
+
+type ToastError = {
+  errorType: 'TOAST',
+  errorCode: string;
+  errorMessage: string;
+  toastShowDuration: number;
+}
+
+type AlertError = {
+  errorType: 'ALERT',
+  errorCode: string;
+  errorMessage: string;
+  onConfirm: () => void;
+}
+
+type ErrorFeedbackType = TextError | ToastError | AlertError;
+
+const errorArr: ErrorFeedbackType[] = [
+  {
+    errorType: 'TEXT', 
+    errorCode: '100', 
+    errorMessage: '텍스트 에러'
+  },
+  {
+    errorType: 'TOAST', 
+    errorCode: '100', 
+    errorMessage: '토스트 에러', 
+    toastShowDuration: 3000
+  },
+  {
+    errorType: 'ALERT', 
+    errorCode: '100', 
+    errorMessage: '얼럿 에러', 
+    onConfirm: () => {}
+  }
+];
+
+// Exhaustiveness Checking 예시
+type ProductPrice = '10000' | '20000' | '5000';
+
+const getProductName = (productPrice: ProductPrice): string => {
+  if(ProductPrice === '1000') return '배민상품권 1만원';
+  if(ProductPrice === '2000') return '배민상품권 2만원';
+  // if(ProductPrice === '5000') return '배민상품권 5천원';
+  else {
+    exhaustiveCheck(productPrice);
+    return '배민상품권';
+  } 
+}
+
+const exhaustiveCheck  = (param: never) => {
+  throw new Error('type error.');
+}
