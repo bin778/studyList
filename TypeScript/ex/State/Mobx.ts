0c3de9d0539c65f7a8dee1f00f8058d1889dd151
CYI3 <bin778@naver.com>
2024-06-11 14:20:04 +0900

    feat Mobx.ts

---
+++ TypeScript/ex/State/Mobx.ts
@@ -0,0 +1,30 @@
+import { observer } from 'mobx-react-lite';
+import { makeAutoObservable } from 'mobx';
+
+class Cart {
+  itemAmount = 0;
+  
+  constructor() {
+    makeAutoObservable(this);
+  }
+  
+  increase() {
+    this.itemAmount += 1;
+  }
+  
+  reset() {
+    this.itemAmount = 0;
+  }
+}
+
+const myCart = new Cart();
+const CartView = observer(() => (
+ <button onClick={()=>cart.reset()}>
+    amount of cart items: {cart.itemAmount}
+ </button>
+));
+
+ReactDOM.render(<CartView cart={myCart} />, document.body);
+setInterval(() => {
+  myCart.increase();
+}, 1000);
